4adcb47ff01c76101c539df75302c8d9eb755a9f
brett ayre <s3403448@student.rmit.edu.au>
2015-07-27 12:37:28 +0000

    new baseline

---
+++ app/Config/database.php
@@ -0,0 +1,20 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'webadmin',
+		'password' => 'Backwards7',
+		'database' => 'thisbank',
+	);
+}
